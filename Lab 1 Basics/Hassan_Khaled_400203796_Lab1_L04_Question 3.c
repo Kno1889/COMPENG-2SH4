ef4dda8efbabf747f529ae30aeaad1ce3a8f5376
Kno1889 <56836650+Kno1889@users.noreply.github.com>
2021-03-16 14:02:35 -0400

    Cleanup

--- Lab 1 Basics/Hassan_Khaled_400203796_Lab1_L04_Question 3.c
+++ Lab 1 Basics/Hassan_Khaled_400203796_Lab1_L04_Question 3.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 int main(){
-    // This program checks if the input value lies between 10,000 and 99,999 (inclusive)
-    // or between -99,999 and -10,000. If it is, then it is a 5-digit number. 
+    // This program checks if the input value is a 5-digit number by checking if it lies between 10,000 and 99,999 (inclusive)
+    // or between -99,999 and -10,000.  
+    
+    
     int x = 1;// x starts at 1 to initiate the while loop, then changes depending on user input.
     while(x != 0){
         printf("Please input an integer (0 to end): \n");
         scanf("%d", &x);
+        
         if(x>=10000 && x<=99999 || x<=-10000 && x>=-99999){
             printf("Your input is a five-digit number: %d \n", x);
         }
